93e97b4a4a22104c022fbd43589496a9d52ac907
smonish64 <smonishsudhakar@gmail.com>
2026-05-08 15:01:57 +0530

    Update s3.py

--- pipeline/s3.py
+++ pipeline/s3.py
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state"
     key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
 }
