b27995a7ae677c2d8a65b1ca08656deabc2fec20
smonish64 <smonishsudhakar@gmail.com>
2026-05-08 15:03:32 +0530

    Change S3 bucket name for production environment

--- pipeline/s3.py
+++ pipeline/s3.py
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state"
+    bucket         = "my-terraform-state-prods"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
