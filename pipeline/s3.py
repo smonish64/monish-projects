880024450dbf77bfc24d3caebd9a15e8964786d6
smonish64 <smonishsudhakar@gmail.com>
2026-05-08 14:55:07 +0530

    Add Terraform S3 backend configuration

---
+++ pipeline/s3.py
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
